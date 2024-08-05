6e61ebf4e9a1f954c1e688507f4f3af1fb89020b
JuanDavidZabalaTapiero <juandavidzabalatapiero@gmail.com>
2024-08-05 16:43:26 -0500

    Añadí la sec de educación y footer

--- src/js/script.js
+++ src/js/script.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
       document.documentElement.style.setProperty("--color-secundario", "#000");
       document.documentElement.style.setProperty("--color-bg", "#f0f0f0");
       document.documentElement.style.setProperty("--color-tercero", "#d4d4d4");
+      document.documentElement.style.setProperty("--color-tercero-oscuro", "#DFDFDF");
     } else if (i_mode.classList.contains("fa-sun")) {
       i_mode.classList.remove("fa-sun");
       i_mode.classList.add("fa-moon");
@@ -20,17 +21,23 @@ document.addEventListener("DOMContentLoaded", () => {
       document.documentElement.style.setProperty("--color-secundario", "#fff");
       document.documentElement.style.setProperty("--color-bg", "#1a1a1a");
       document.documentElement.style.setProperty("--color-tercero", "#2b2b2b");
+      document.documentElement.style.setProperty("--color-tercero-oscuro", "#202020");
     }
   });
 
   // ACCORDEÓN
   document.querySelectorAll(".div_accordion_head").forEach(function (header) {
+
     header.addEventListener("click", function () {
       var content = this.nextElementSibling;
 
       if (content.style.display === "block") {
+        // QUITO LA CLASE OPEN AL DIV HEAD
+        header.classList.remove("open");
         content.style.display = "none";
       } else {
+        // PONGO LA CLASE OPEN AL DIV HEAD
+        header.classList.add("open");
         content.style.display = "block";
       }
     });
